f026fb7c0b5ccaa561c366a8af1e9b98ac87209b
Hiroyuki Inoue <syslink.h.inoue@gmail.com>
2013-10-28 06:38:28 +0900

    画像を挿入

--- app/assets/javascripts/slides.js
+++ app/assets/javascripts/slides.js
@@ -1,5 +1,6 @@
 (function (window) {
 
+<<<<<<< HEAD
 // Model
 var 
     // スライド
@@ -56,4 +57,58 @@ SectionView = Backbone.View.extend({
 });
 
 
+=======
+var 
+    // 画像
+    Image,
+    // 画像一覧
+    Images,
+    // 画像表示
+    ImageView,
+    // 画像一覧表示
+    ImagesView;
+
+// 画像
+Image = Backbone.Model.extend({});
+
+// 画像一覧
+Images = Backbone.Collection.extend({
+  model: Image,
+  url: '/images'
+});
+
+// 
+ImageView = Backbone.View.extend({
+  template: _.template($("#image-template").text()),
+  events: {
+    'click a': 'onClick'
+  },
+  onClick: function (e) {
+    e.preventDefault();
+    $("#page_content").val($('#page_content').val() + '<img src="' + this.model.get('medium') + '"></img>');
+    $("#modal-close").click();
+  },
+  render: function () {
+    this.$el.html(this.template(this.model.attributes));
+    return this;
+  }
+});
+
+// 
+ImagesView = Backbone.View.extend({
+  el: "#images"
+});
+
+var images = new Images();
+var imagesView = new ImagesView();
+images.fetch({
+  success: function () {
+    images.each(function (image) {
+      var imageView = new ImageView({model: image});
+      imagesView.$el.append(imageView.render().el);
+    });
+  }
+});
+
+>>>>>>> issue19
 })(this);
